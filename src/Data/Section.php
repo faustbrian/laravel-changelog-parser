12aefb64113ee492cd19c296bc04c8e206b5fd33
Brian Faust <hello@basecode.sh>
2023-04-15 16:38:33 +0300

    perf: parse section content only once

--- src/Data/Section.php
+++ src/Data/Section.php
@@ -8,12 +8,20 @@ use Spatie\LaravelData\Data;
 
 final class Section extends Data
 {
+    public readonly array $items;
+
     public function __construct(
         public readonly string $type,
         public readonly string $content,
         public readonly ?string $description = null,
     ) {
-        //
+        $this->items = \array_map(
+            fn (string $item) => \trim(\mb_substr($item, 1)),
+            \array_filter(
+                \explode("\n", $this->content),
+                fn (string $item) => \str_starts_with($item, '-'),
+            ),
+        );
     }
 
     public function getType(): string
@@ -28,12 +36,6 @@ final class Section extends Data
 
     public function getItems(): array
     {
-        return \array_map(
-            fn (string $item) => \trim(\mb_substr($item, 1)),
-            \array_filter(
-                \explode("\n", $this->content),
-                fn (string $item) => \str_starts_with($item, '-'),
-            ),
-        );
+        return $this->items;
     }
 }
